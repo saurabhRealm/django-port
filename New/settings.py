848ec2cbb583be178eaa9e6f0c10e997501018f4
Saurabh Mathur <saurabhmathur1365@gmail.com>
2025-01-03 23:21:07 +0530

    s

--- New/settings.py
+++ New/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = [
+    'django-1-n2s4.onrender.com',
+    # Add other allowed hosts if necessary
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
